2096051cbb21041eabd81af27088af4d696a261f
carojaspaz <carojaspaz@gmail.com>
2018-01-30 14:14:08 -0500

    201801301413 Class 01

--- Lesson01/src/app/app.component.ts
+++ Lesson01/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My first Angular app - Carlos Rojas';
+  title = 'Tour of Heroes - Lesson 01';
 }
