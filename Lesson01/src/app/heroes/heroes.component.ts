2eb72ac3d0a63af9c9abedd2ffe6e3bcd8d1e236
carojaspaz <carojaspaz@gmail.com>
2018-01-31 16:14:13 -0500

    201801311614 Class 03

--- Lesson01/src/app/heroes/heroes.component.ts
+++ Lesson01/src/app/heroes/heroes.component.ts
@@ -9,13 +9,9 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-  heroes : Hero[];  
-  
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService) { 
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -23,10 +19,21 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
